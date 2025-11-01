4b23edf592a48540b8e60dacda59e91f0ec1f1a9
erick moses <moseserickomondi@gmail.com>
2025-11-01 12:01:57 +0300

    removed the clean backend folder

--- clean-backend/app/models/__init__.py
+++
@@ -1,7 +0,0 @@
-from .user import User
-from .crop import Crop
-from .disease import Disease
-from .treatment import Treatment
-from .report import Report
-
-__all__ = ['User', 'Crop', 'Disease', 'Treatment', 'Report']
